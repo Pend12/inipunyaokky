33c550c4d9ed8f67d44692b2009e9a849457cd22
ASUS <ASUS@LAPTOP-E7ROLCGO>
2021-12-03 10:46:27 +0700

    Add project files.

---
+++ Trytry/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Trytry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka: ");
+            int bil = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            for (int i = 2; i <= bil / 2; i++)
+            {
+                if (bil % i == 0)
+                {
+                    Console.Write("Angka bukan prima");
+                    counter = 1;
+                    break;
+                }
+            }
+            if (counter == 0)
+                Console.Write("Angka prima");
+        }
+        }
+    }
